fc350ee979e8f4809375fcdfd334b088c3a90d3e
painelradar <painelradar@gmail.com>
2023-03-03 18:36:42 -0300

    fix panel

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -140,6 +140,7 @@
         };
 
         const voices = speechSynthesis.getVoices();
+        const voice = voices[0];
 
         const speak = async text => {
             const message = new SpeechSynthesisUtterance(text)
@@ -183,19 +184,6 @@
             }, interval);
         }
         chamaInterVal(1000);
-        function populateVoiceList() {
-        if (typeof speechSynthesis === 'undefined') {
-        return;
-        }
-
-        const voices = speechSynthesis.getVoices();
-
-        for (let i = 0; i < voices.length; i++) { const option=document.createElement('option');
-            option.textContent=`${voices[i].name} (${voices[i].lang})`; if (voices[i].default) { option.textContent
-            +=' — DEFAULT' ; } option.setAttribute('data-lang', voices[i].lang); option.setAttribute('data-name',
-            voices[i].name); document.getElementById("voiceSelect").appendChild(option); } } populateVoiceList(); if (typeof
-            speechSynthesis !=='undefined' && speechSynthesis.onvoiceschanged !==undefined) {
-            speechSynthesis.onvoiceschanged=populateVoiceList; }
     </script>
 </body>
 
